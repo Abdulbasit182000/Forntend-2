3382a47dac82195a42d04836266a7e5662acacd7
AbdulBasit182000 <abdulbasit.jalil@gmail.com>
2023-12-29 14:33:01 +0500

    Using Props to send data form one component to another

--- firstapp/src/Cpmponents/newhome.js
+++ firstapp/src/Cpmponents/newhome.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import BlogList from "./BlogList";
 
 const Newhome = () => {
     const [blogs, setBlogs] = useState([
@@ -37,12 +38,7 @@ const Newhome = () => {
                 <button onClick={handleClick}>Submit</button>
             </div>
             <div></div>
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs!"/>
         </div>
     );
 }

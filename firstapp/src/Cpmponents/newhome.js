a7df2d5d5e25789c8344ec1c94437602cfca5738
AbdulBasit182000 <abdulbasit.jalil@gmail.com>
2023-12-29 16:48:49 +0500

    Ouputted a loading message to simulate data being feteched from a server rathar than our local storage

--- firstapp/src/Cpmponents/newhome.js
+++ firstapp/src/Cpmponents/newhome.js
@@ -1,21 +1,27 @@
 import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import { isCompositeComponent } from "react-dom/test-utils";
 
 const Newhome = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true)
     
     useEffect(() => {
+        setTimeout( () => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
             return res.json()
         })
         .then(data => {
             setBlogs(data);
-        })
+            setIsLoading(false);
+        });
+        },1000);
     },[]);
 
     return (
         <div className="newhome">
+            {isLoading && <div> Loading... </div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );

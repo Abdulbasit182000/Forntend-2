fef8dcd5ff8a38ad38831b8ed2ff02843109bbd8
AbdulBasit182000 <abdulbasit.jalil@gmail.com>
2024-01-01 19:12:33 +0500

    Can now add new items to database using post request

--- firstapp/src/Cpmponents/BlogDetails.js
+++ firstapp/src/Cpmponents/BlogDetails.js
@@ -1,10 +1,19 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../usefetch";
 
 const BlogDetails = () => {
     const {id} = useParams();
+    const {data: blog , isLoading: isLoading} = useFetch('http://localhost:8000/blogs/' + id);
     return ( 
         <div className="blog-details">
-            <h2>Blog details - {id} </h2>
+            {isLoading && <div>Loading....</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
      );
 }
